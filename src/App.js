01ac4888ae1cb2c47d3b742f8411a1a667cdfc39
frankc95 <49732467+frankc95@users.noreply.github.com>
2021-07-11 18:47:27 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ import '../src/styles/App.css';
 import Menu from './Components/Menu';
 import Button from './Components/Buttons';
 
+const allMonths = ['All', ...new Set(items.map(item => item.publishedDate.slice(5, 7)))]
+
 function App() {
   const [menuItem, setMenuItem] = useState(items);
-  const [buttons, setButtons] = useState([]);
+  const [buttons, setButtons] = useState(allMonths);
+
+  const filter = (button) => {
+    if (button === 'All')
+    {
+      setMenuItem(items)
+      return;
+    }
+
+
+    const filteredData = items.filter(item => item.publishedDate.slice(5, 7) === button);
+    setMenuItem(filteredData)
+  }
 
   return (
     <>
       <div className="title">
         <h1>
-        Portfolio 
+          Portfolio
           <span> Filter</span>
         </h1>
       </div>
-      <Button />
+      <Button button={buttons} filter={filter} />
       <Menu menuItem={menuItem} />
     </>
   );
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Menu = () => {
+const Menu = ({menuItem}) => {
     return (
-        <div>
-            
+        <div className='allWrap'>
+            {
+                menuItem.sort().map((item) => {
+                    return <div className="item-con" key={item.id}>
+                        <div className="item-container">
+                            <h2>{item.title}</h2>
+                            <p>{item.body}</p>
+                            <small>{item.publishedDate.slice(5,7)}</small>
+                        </div>
+                    </div>
+                })
+            }
         </div>
     )
 }
