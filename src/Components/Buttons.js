8c4397b95860cf5a222cf0ab15fcbcee4c75a9df
frankc95 <blazewicz.j@gmail.com>
2021-07-12 21:34:46 +0100

    first

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Buttons = () => {
+function getMonthName(monthVar) {
+    const d = new Date();
+    d.setMonth(monthVar - 1);
+    const monthName = d.toLocaleString('default', { month: 'short' });
+    return monthName;
+}
+
+const Buttons = ({ button, filter }) => {
     return (
         <div>
-            
+            {/* <button type="button" onClick={() => filter('02')}>February</button> */}
+            {
+                button.map((month, i) => {
+                    return <button type='button' onClick={() => filter(month)} key={i}>{
+                        month === 'All' ? month : getMonthName(month)
+                    }</button>
+                })
+            }
         </div>
     )
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const Menu = () => {
+function getMonthName(monthVar) {
+    const d = new Date();
+    d.setMonth(monthVar - 1);
+    const monthName = d.toLocaleString('default', { month: 'short' });
+    return monthName;
+}
+
+const Menu = ({ menuItem }) => {
     return (
-        <div>
-            
+        <div className='allWrap'>
+            {
+                menuItem.sort().map((item) => {
+                    return <div className="item-con" key={item.id}>
+                        <div className="item-container">
+                            <h2>{item.title}</h2>
+                            <p>{item.body}</p>
+                            <small>{getMonthName(item.publishedDate.slice(5, 7))}</small>
+                        </div>
+                    </div>
+                })
+            }
         </div>
     )
 }
